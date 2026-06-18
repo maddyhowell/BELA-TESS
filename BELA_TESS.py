ab62089f7a2b5645c22d10ca7ddfa5c1da818d21
Maddy Howell <53502531+maddyhowell@users.noreply.github.com>
2026-06-18 15:41:15 -0400

    update scaling factor for points in tpf widget

--- BELA_TESS.py
+++ BELA_TESS.py
@@ -184,7 +184,12 @@ class TPFMaskSelector:
 
                         continue
                 else:
-                    fac=64.0 / 2 ** (g_mag / 5.0)
+                    scale_fac = self.gmag/row.Gmag
+                    # print(scale_fac)
+                    # fac = 150 - 250*(1- scale_fac)
+                    fac = 40 - 200*(1- scale_fac)
+
+                    fac=64.0 / 2 ** (self.gmag / 5.0)
 
                     if fac < 0:
                         fac = 1
